52265159799a7daced607863b5909ebe47490b5d
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-27 01:31:43 +0530

    Doing CRUD operation on File system

--- node-tut/index3.js
+++ node-tut/index3.js
@@ -5,4 +5,21 @@ const path=require('path');
 const dirPath=path.join(__dirname,'crud');
 const filePath=`${dirPath}/apple.txt`;
 
-fs.writeFileSync(filePath,"This is a simple text file !!");
+
+//creating file
+//fs.writeFileSync(filePath,"This is a simple text file !!");
+
+//Reading the file
+//fs.readFile(filePath,'utf8',(err,item)=>{
+//    console.log(item);
+//})
+
+//updating a file
+
+//fs.appendFile(filePath,"and file name is apple.txt !!",(err)=>{
+//    if(!err) console.log("File is Updated !!!");
+//});
+
+
+//deleting the file
+fs.unlinkSync(filePath);
