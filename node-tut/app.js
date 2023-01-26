e0fa6bfd53289f8943ed9e0e59cb66f2aefe6d39
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-26 17:29:51 +0530

    Started making a very basic practise project

--- node-tut/app.js
+++ node-tut/app.js
@@ -4,5 +4,8 @@ module.exports={
     //so we will not use "=" to assign values to variable
     //but we will use ":" to assign values to the variables
     x:10,
-    y:20
+    y:20,
+    z:function(){
+        return "This is a function !!";
+    }
 };
--- node-tut/index.js
+++ node-tut/index.js
@@ -9,13 +9,20 @@ var b=30;
 if(a===20){
     console.log("matched !!");
 } 
-
-const arr=[1,2,3,4,5,6,7,8,9];
-console.warn(arr);
 //importing the data from different files 
 const app=require('./app');
 console.log("x="+app.x);
 console.warn("y="+app.y);
+console.log("z="+app.z());
+
+const arr=[1,2,3,4,5,6,7,8,9];
+
+let result=arr.filter((item)=>{
+    return (item%2==0);
+
+});
+console.log(result);
+
 
 
 
