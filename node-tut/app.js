2aab745de66057ca816542643a9adb807111ce8a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-01-26 14:16:43 +0530

    Started making a very basic practise project

---
+++ node-tut/app.js
@@ -0,0 +1,8 @@
+module.exports={
+    //making an object
+    //since it is inside the object 
+    //so we will not use "=" to assign values to variable
+    //but we will use ":" to assign values to the variables
+    x:10,
+    y:20
+};
--- node-tut/index.js
+++ node-tut/index.js
@@ -1,5 +1,21 @@
-console.log("Hello !!!");
+//Trying basic coding syntax
+//Just brushing the Javascript concepts
 
+
+console.log("Hello !!!");
 var a=20;
 var b=30;
-console.log(a+b);
+//console.log(a+b);
+if(a===20){
+    console.log("matched !!");
+} 
+
+const arr=[1,2,3,4,5,6,7,8,9];
+console.warn(arr);
+//importing the data from different files 
+const app=require('./app');
+console.log("x="+app.x);
+console.warn("y="+app.y);
+
+
+
