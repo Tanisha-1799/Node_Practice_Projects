fb2ce99a7077557f1a9ad33cd2bb516189c74eea
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-08 19:08:13 +0530

    Making the signUp page

--- E-CommerceDashboard/frontend/src/components/SignUp.js
+++ E-CommerceDashboard/frontend/src/components/SignUp.js
@@ -1,13 +1,34 @@
-import React from 'react';
+import React,{useState} from 'react';
 
 const SignUp=()=>{
+
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("");
+
+    const collectData=()=>{
+        alert(name+" "+email+" "+password);
+    }
+
     return(
         <div className='container-style'>
             <h1 className='heading-style'>Register</h1>
-            <input className='input-style' type="text" placeholder="Enter Your Name" />
-            <input className='input-style' type="text" placeholder="Enter Your Email" />
-            <input className='input-style' type="password" placeholder="Enter Password" />
-            <button className='button-style'>SignUp</button>
+            <input className='input-style' 
+            type="text"
+            value={name} 
+            onChange={(e)=>setName(e.target.value)} 
+            placeholder="Enter Your Name" />
+            <input className='input-style' 
+            type="text" 
+            value={email} 
+            onChange={(e)=>setEmail(e.target.value)} 
+            placeholder="Enter Your Email" />
+            <input className='input-style' 
+            type="password" 
+            value={password} 
+            onChange={(e)=>setPassword(e.target.value)} 
+            placeholder="Enter Password" />
+            <button className='button-style' onClick={collectData}>SignUp</button>
         </div>
     )
 }
