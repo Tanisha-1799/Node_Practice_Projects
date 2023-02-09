5726c4e858d675771364864ee1f82ec60add663b
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-09 18:42:10 +0530

    Connecting to the database

--- E-CommerceDashboard/Backend/index.js
+++ E-CommerceDashboard/Backend/index.js
@@ -1,16 +1,10 @@
 const express=require('express');
-const mongoose=require('mongoose');
+require('./db/config');
+const User=require('./db/User');
 
 const app=express();
 
 
-const connectDB=async ()=>{
-    mongoose.connect("mongodb://localhost:27017/e-comm");
-    const productSchema=new mongoose.Schema({});
-    const product=mongoose.model('products',productSchema);
-    const data=await product.find();
-    console.warn(data);
-}
-connectDB();
+
 
 app.listen(5000);
