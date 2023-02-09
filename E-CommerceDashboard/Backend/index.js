2a8f269860d0f659331f7b9fa29706d5f9bef77a
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-09 18:54:00 +0530

    Creating sign up api

--- E-CommerceDashboard/Backend/index.js
+++ E-CommerceDashboard/Backend/index.js
@@ -3,8 +3,13 @@ require('./db/config');
 const User=require('./db/User');
 
 const app=express();
+app.use(express.json());
 
-
+app.post("/register",async (req,res)=>{
+    let user=new User(req.body);
+    let result=await user.save();
+    res.send(result);
+})
 
 
 app.listen(5000);
