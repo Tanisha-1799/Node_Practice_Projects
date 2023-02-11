e0e17d0f2ec18e0c3d3ec0ba50955d9d26b25545
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-11 16:43:50 +0530

    Integrating login Api with the frontend

--- E-CommerceDashboard/frontend/src/components/LogIn.js
+++ E-CommerceDashboard/frontend/src/components/LogIn.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {useNavigate} from 'react-router-dom';
 
 const LogIn=()=>{
     const [email,setEmail]=useState("");
     const [password,setPassword]=useState("");
     const navigate=useNavigate();
+
+    useEffect(()=>{
+        const auth=localStorage.getItem("user");
+        if(auth){
+            navigate('/')
+        }
+    },[]);
 
     const handleLogin= async ()=>{
         let result=await fetch("http://localhost:5000/login",{
