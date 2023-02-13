d28edad7a8b68909c5e815ac9e00a509998bdcac
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-13 14:11:37 +0530

    Integrating the add product api with the frontend

--- E-CommerceDashboard/frontend/src/components/AddProducts.js
+++ E-CommerceDashboard/frontend/src/components/AddProducts.js
@@ -6,8 +6,24 @@ const AddProducts=()=>{
     const [category,setCategory]=useState('');
     const [company,setCompany]=useState('');
 
-    const addproduct=()=>{
-        alert(name+" "+price+" "+category+" "+company);
+    const addproduct= async()=>{
+       // alert(name+" "+price+" "+category+" "+company);
+       //integrating api here, sending data to the database
+        //taking the id of the current user from local storage
+        const userId=JSON.stringify(localStorage.getItem("user"))._id;
+        let result=await fetch('http://localhost:5000/add-product',{
+            method:'post',
+            body:JSON.stringify({name,price,category,company}),
+            headers:{
+                'Content-Type':'application/json'
+            },
+           })
+           result=await result.json();
+           console.warn(result);
+           //storing data in the local storage of the browser
+           //localStorage.setItem("user",JSON.stringify(result));
+
+
     }
 
     return(
