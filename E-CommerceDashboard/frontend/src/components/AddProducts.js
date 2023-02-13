621c052a5f75e9467c2d9b6f9cc1ab72a147a6ea
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-13 14:01:27 +0530

    Making the add product component

--- E-CommerceDashboard/frontend/src/components/AddProducts.js
+++ E-CommerceDashboard/frontend/src/components/AddProducts.js
@@ -6,6 +6,10 @@ const AddProducts=()=>{
     const [category,setCategory]=useState('');
     const [company,setCompany]=useState('');
 
+    const addproduct=()=>{
+        alert(name+" "+price+" "+category+" "+company);
+    }
+
     return(
         <div className='add-product'>
             <h1 className='heading-style'>Add Products</h1>
@@ -21,7 +25,7 @@ const AddProducts=()=>{
             <input type="text" className='input-style' placeholder="Enter product company" 
                 onChange={(e)=>setCompany(e.target.value)} value={company}
             />
-            <button className='button-style'>Add Product</button>
+            <button onClick={addproduct} className='button-style'>Add Product</button>
 
 
         </div>
