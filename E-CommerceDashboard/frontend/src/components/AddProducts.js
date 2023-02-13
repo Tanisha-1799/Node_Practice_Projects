0f1b90cf610d57d364e085e1d0ae53b27bd86280
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-13 13:57:30 +0530

    Making the add product component

--- E-CommerceDashboard/frontend/src/components/AddProducts.js
+++ E-CommerceDashboard/frontend/src/components/AddProducts.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddProducts=()=>{
+    const [name,setName]=useState('');
+    const [price,setPrice]=useState('');
+    const [category,setCategory]=useState('');
+    const [company,setCompany]=useState('');
+
     return(
         <div className='add-product'>
             <h1 className='heading-style'>Add Products</h1>
-            <input type="text" className='input-style' placeholder="Enter product name" />
-            <input type="text" className='input-style' placeholder="Enter product price" />
-            <input type="text" className='input-style' placeholder="Enter product category" />
-            <input type="text" className='input-style' placeholder="Enter product company" />
+            <input type="text" className='input-style' placeholder="Enter product name" 
+                onChange={(e)=>setName(e.target.value)} value={name}
+            />
+            <input type="text" className='input-style' placeholder="Enter product price" 
+                onChange={(e)=>setPrice(e.target.value)} value={price}
+            />
+            <input type="text" className='input-style' placeholder="Enter product category" 
+                onChange={(e)=>setCategory(e.target.value)} value={category}
+            />
+            <input type="text" className='input-style' placeholder="Enter product company" 
+                onChange={(e)=>setCompany(e.target.value)} value={company}
+            />
             <button className='button-style'>Add Product</button>
 
 
