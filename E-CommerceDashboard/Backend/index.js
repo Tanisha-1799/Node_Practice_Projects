b95da9f90e2a2ff8482a1993ab410666c7a015d2
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-14 13:20:01 +0530

    Making list product api

--- E-CommerceDashboard/Backend/index.js
+++ E-CommerceDashboard/Backend/index.js
@@ -35,6 +35,17 @@ app.post('/add-product',async (req,res)=>{
     let product=new Product(req.body);
     let result=await product.save();
     res.send(result);
+});
+
+app.get('/products',async (req,res)=>{
+    let products=await Product.find();
+    if(products.length>0){
+        res.send(products)
+    }
+    else{
+        res.send({result:"No products found !!"});
+    }
+
 })
 
 
