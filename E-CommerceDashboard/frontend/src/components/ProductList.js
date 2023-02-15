da324d35bbbfd7262de398ab2ae5f4ae387d49e7
Tanisha-1799 <triveditanisha01171999@gmail.com>
2023-02-15 18:56:35 +0530

    Integrating delete api to the frontend

--- E-CommerceDashboard/frontend/src/components/ProductList.js
+++ E-CommerceDashboard/frontend/src/components/ProductList.js
@@ -13,6 +13,17 @@ const ProductList = () => {
     setProducts(result);
   };
 
+  const deleteProduct=async (id)=>{
+    let result=await fetch(`http://localhost:5000/product/${id}`,{
+      method:"Delete"
+    });
+    result=await result.json();
+    if(result){
+      getProducts();
+    }
+
+  }
+
   return (
     <div className="product-List">
       <h1>Product List </h1>
@@ -22,14 +33,16 @@ const ProductList = () => {
         <li><h3>Price</h3></li>
         <li><h3>Category</h3></li>
         <li><h3>Company</h3></li>
+        <li><h3>Operation</h3></li>
       </ul>
       {products.map((item,index) => 
-        <ul>
+        <ul key={item._id}>
           <li>{index+1}</li>
           <li>{item.name}</li>
           <li>$ {item.price}</li>
           <li>{item.category}</li>
           <li>{item.company}</li>
+          <li><button onClick={()=>deleteProduct(item._id)}>Delete</button></li>
         </ul>
       )}
     </div>
